255fff45fd13117402915259c01c2daf7ff1ec9c
Yves Riedener <git@reecube.com>
2017-09-02 16:28:43 +0200

    implemented easy_admin and adminbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+            // symfony base
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -15,6 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // extensions
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            // custom platform
+            new AdminBundle\AdminBundle(),
             new AppBundle\AppBundle(),
         ];
 
